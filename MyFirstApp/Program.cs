5083f162cf7f5523b9244a973de9c1a3185281c3
Perlebach <markenkeres@gmail.com>
2024-03-03 18:04:48 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisesta tema nime
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name");
+//string- sõne
+string userName;
+
+
+userName= Console.ReadLine();
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName!}!");
